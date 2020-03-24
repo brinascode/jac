b11a2cbb4ce8641cc88f31e66e3d755d1752ffaa
Sabrina Koumoin <sak96528@marymount.edu>
2020-03-24 11:04:47 +0000

    created header

---
+++ client/src/components/Banner.jsx
@@ -0,0 +1,6 @@
+import React from "react"
+export default function Banner(){
+    return(
+        <img src="/images/banner.png" alt="banner" width="100%"/>
+    )
+}
---
+++ client/src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+
+var headerStyle={
+    fontSize:"3vw",
+    textAlign:"left",
+    padding:"1vw"
+}
+export default function Header(){
+        return(
+            <header style={headerStyle}>
+                <strong>Jesus above Corona</strong>
+            </header>
+        )
+}
